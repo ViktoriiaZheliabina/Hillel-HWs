e939f26b156527baec7ad86c627e07ca06f661cb
ViktoriiaZheliabina <112900464+ViktoriiaZheliabina@users.noreply.github.com>
2023-01-01 19:41:50 +0200

    Add files via upload

---
+++ HW1/HW1.py
@@ -0,0 +1,26 @@
+# Задача 1.
+
+first = 10
+second = 30
+print('Task 1:')
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+print(first // second)
+print(first % second)
+print(first ** second)
+print(' ')
+
+# Задача 2.
+
+
+print('Task 2:')
+comparison_res = first < second
+print(comparison_res)
+comparison_res1 = first > second
+print(comparison_res1)
+comparison_res2 = first == second
+print(comparison_res2)
+comparison_res3 = first != second
+print(comparison_res3)
