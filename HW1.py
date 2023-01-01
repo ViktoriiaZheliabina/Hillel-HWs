ed117b6c837912912052fcb4edec25af5ed8249e
Viktoriia Zhelibina <victoria.zh81@gmail.com>
2023-01-01 18:41:53 +0200

    HW1

---
+++ HW1.py
@@ -0,0 +1,26 @@
+# Задача 1.
+
+first = 10
+second = 30
+print('Task 1:')
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+print(first // second)
+print(first % second)
+print(first ** second)
+print(' ')
+
+# Задача 2.
+
+var = 10
+print('Task 2:')
+print(var < first)
+print(var > first)
+print(var == first)
+print(var != first)
+print(var < second)
+print(var > second)
+print(var == second)
+print(var != second)
