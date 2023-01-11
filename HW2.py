c7db2cc42b11c6a7043105a5ea2464462be3a1e0
Viktoriia Zhelibina <victoria.zh81@gmail.com>
2023-01-12 01:00:41 +0200

    update

--- HW2.py
+++ HW2.py
@@ -1,24 +1,25 @@
 # Задача 1.
 
-# strg = 'Word {} has {} letters'
-# a = input()
-# res = strg.format(a, len(a))
-# print(res)
+strg = 'Word {} has {} letters'
+a = input()
+res = strg.format(a, len(a))
+print(res)
 
 # Задача 2.
 
 print('Enter your age, please:')
 a = input()
-if a.isdigit() == False:
+if not a.isdigit():
     print('Incorrect value')
 else:
+    ia = int(a)
     if '7' in a:
         print('You are lucky today!')
-    elif int(a) < 7:
+    elif ia < 7:
         print('Where are your parents?')
-    elif int(a) < 16:
+    elif ia < 16:
         print('This is a movie for adults!')
-    elif int(a) > 65:
+    elif ia > 65:
         print('Show your pension certificate!')
     else:
         print('There are no tickets anymore!')
