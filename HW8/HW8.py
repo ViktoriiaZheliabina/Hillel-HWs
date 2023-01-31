cd54d20b85446455c287793a77daa9a52d89931e
Viktoriia Zhelibina <victoria.zh81@gmail.com>
2023-01-31 22:49:06 +0200

    HW8 updated

--- HW8/HW8.py
+++ HW8/HW8.py
@@ -4,11 +4,12 @@
 # а на вихід: ['ALFRED', 'BEN', 'WILLIAM']
 
 
-def upper_foo():
-    list_low = ['alfred', 'ben', 'william']
+def upper_foo(list_low):
     list_upper = list(map(lambda x: x.upper(), list_low))
     return list_upper
-print(upper_foo())
+
+
+print(upper_foo(['alfred', 'ben', 'william']))
 
 
 # 2. Напишіть функцію, яка на вхід буде приймати список флоатів, а в результаті
@@ -18,11 +19,12 @@ print(upper_foo())
 # Наприклад на вхід: [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59]
 # а на вихід: [18.922, 37.088, 10.562, 95.453, 4.666, 78.854, 21.068]
 
-def degree_foo():
-    list_input = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59]
+def degree_foo(list_input):
     list_degree = list(map(lambda x: round(x**2), list_input))
     return list_degree
-print(degree_foo())
+
+
+print(degree_foo([4.35, 6.99, 3.25, 9.77, 2.16, 8.88, 4.59]))
 
 
 # 3. Напишіть функцію, яка на вхід буде приймати два списки, а в результаті повертати список
@@ -31,12 +33,12 @@ print(degree_foo())
 # Наприклад на вхід: ['a', 'b', 'c', 'd', 'e'], [1,2,3,4,5]
 # а на вихід: [('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)]
 
-def union_to_tuple():
-    list_input1 = ['a', 'b', 'c', 'd', 'e']
-    list_input2 = [1, 2, 3, 4, 5]
+def union_to_tuple(list_input1, list_input2):
     list_tuple = dict(zip(list_input1, list_input2))
     return list_tuple
-print(union_to_tuple())
+
+
+print(union_to_tuple(['a', 'b', 'c', 'd', 'e'], [1, 2, 3, 4, 5]))
 
 
 # 4. Напишіть функцію, яка на вхід буде приймати список стрінгів і число,
@@ -46,11 +48,11 @@ print(union_to_tuple())
 # а на вихід: ["Jeff", "Alex", "Anna"]
 
 
-def list_string_less_5():
-    list_inp = ["Jeff", "Alex", "Jonathan", "Richelle", "Anna"]
-    num = 5
+def list_string_less_num(list_inp, num):
     list_out = list(filter(lambda k: len(k) < num, list_inp))
     return list_out
-print(list_string_less_5())
+
+
+print(list_string_less_num(["Jeff", "Alex", "Jonn", "Richelle", "Anna"], 5))
 
 
