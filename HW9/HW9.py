6178a7fda4c3138e14918660d6b2cef16188e6b2
Viktoriia Zhelibina <victoria.zh81@gmail.com>
2023-02-26 00:03:48 +0200

    HW9

---
+++ HW9/HW9.py
@@ -0,0 +1,60 @@
+# Завдання 1.
+# Створіть клас з описом будь-якої компанії. Наприклад Hillel, Epam, Google.
+# Так як ми робили з людиною, тобто клас Company, що буде універсальним.
+
+# Створіть клас з описом будь-якого працівника(Типу універсальний). Employee.
+
+# Поки немає необхідності зв'язувати перший і другий класи.
+
+# Намагайтесь максимально повно описати класи на предмет полів і методів.
+# Додайте анотації до всіх методів. До кожного метода обов'язково треба додати док стрінгу
+# з описом метода і анотації.
+
+class Company:
+    branches = []
+
+    def __init__(self, name: str, type: str, number_of_employees: int = None):
+        self.name = name
+        self.type = type
+        self.number_of_employees = number_of_employees
+
+    def get_name(self):
+        """Метод повертає ім'я об'єкту із класу Company"""
+        return self.name
+
+    def get_type(self):
+        """Метод повертає тип (product, outsource, outstuff etc) об'єкту із класу Company"""
+        return self.type
+
+    def get_number_of_employees(self):
+        """Метод повертає кількість співробітників об'єкту із класу Company"""
+        return self.number_of_employees
+
+    def add_number_of_employees(self, number_of_employees):
+        """Метод дозволяє задати кількість співробітників об'єкту із класу Company"""
+        self.number_of_employees = number_of_employees
+
+    @classmethod
+    def add_branches(cls, branch: str):
+        """Метод задає назву відділення об'єкту із класу Company"""
+        return cls.branches.append(branch)
+
+
+class Employee:
+    def __init__(self, name: str, surname: str, age: int):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+    def get_name(self):
+        """Метод повертає ім'я об'єкту із класу Employee"""
+        return self.name
+
+    def get_surname(self):
+        """Метод повертає прізвище об'єкту із класу Employee"""
+        return self.surname
+
+    def get_age(self):
+        """Метод повертає вік об'єкту із класу Employee"""
+        return self.age
+
