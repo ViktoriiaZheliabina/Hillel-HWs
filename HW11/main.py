a25bf4b2a0c83ea527df8bd25a673134a829d54e
Viktoriia Zhelibina <victoria.zh81@gmail.com>
2023-04-11 23:08:17 +0300

    HW11 Update

--- HW11/main.py
+++ HW11/main.py
@@ -8,6 +8,15 @@ from abc import ABC, abstractmethod
 class Notebook(ABC):
     """Class that describe notebook object"""
     def __init__(self, model_name: str, cpu: str, ram: int, hdd: int, display: float, price: float):
+        """
+
+        :param model_name: Notebook name
+        :param cpu: CPU name
+        :param ram: memory
+        :param hdd: HDD size
+        :param display: diagonal length
+        :param price: price
+        """
         self.model_name = model_name
         self.cpu = cpu
         self.ram = ram
@@ -24,66 +33,57 @@ class Notebook(ABC):
 
     @abstractmethod
     def get_model_name(self):
-        """Method that describes notebook model"""
+        """
+        Method that describes notebook model name
+        :return
+        model name: str
+        """
         pass
 
-    def print_characteristics(self):
-        """Method prints notebook charactristics"""
-        print(self.get_model_name())
-        print(f"  cpu: {self.cpu}")
-        print(f"  ram: {self.ram} Gb")
+    def get_characteristics(self):
+        """
+        Method gets notebook characteristics
+        returns:
+        list of characteristics: str
+        """
+
+        txt = f"{self.get_model_name()}\n"
+        txt += f"  cpu: {self.cpu}\n"
+        txt += f"  ram: {self.ram} Gb\n"
         if self.__is_hdd_present():
-            print(f"  HDD: {self.hdd} Gb")
+            txt += f"  HDD: {self.hdd} Gb\n"
         else:
-            print(f"  HDD: none")
-        print(f"  display: {self.display}\"")
-        print(f"  price: {self.price} грн")
-
-
-class SSD:
-    """Class describes SSD"""
-    def __init__(self, size):
-        self.__size = size
-
-    def get_ssd_size(self):
-        """Method returns SSD size"""
-        if self.__size < 0:
-            return 0
-        else:
-            return self.__size
+            txt += "  HDD: none\n"
+        txt += f"  display: {self.display}\"\n"
+        txt += f"  price: {self.price} грн\n"
+        return txt
 
 
 class Sony(Notebook):
     """Class describes Sony model"""
-    def __init__(self, model_name: str, cpu: str, ram: int, hdd: int, display: float, price: float):
+    def __init__(self,  model_name: str, cpu: str, ram: int, hdd: int, display: float, price: float):
         super().__init__(model_name, cpu, ram, hdd, display, price)
 
     def get_model_name(self):
         return "Sony " + self.model_name
 
 
-class Lenovo(Notebook, SSD):
+class Lenovo(Notebook):
     """Class describes Lenovo model"""
     def __init__(self, model_name: str, cpu: str, ram: int, hdd: int, ssd: int, display: float, price: float):
-        Notebook.__init__(self, model_name, cpu, ram, hdd, display, price)
-        SSD.__init__(self, ssd)
+        super().__init__(model_name, cpu, ram, hdd, display, price)
+        self.ssd = ssd
 
     def get_model_name(self):
         return "Lenovo " + self.model_name
 
-    def print_characteristics(self):
-        Notebook.print_characteristics(self)
-        print(f"  SSD: {SSD.get_ssd_size(self)}")
-
-
-def get_model_name(self):
-    return "  Lenovo " + self.model_name
+    def get_characteristics(self):
+        return super().get_characteristics() + f"  SSD: {self.ssd} Gb\n"
 
 
 if __name__ == '__main__':
-    lenovoIdeaPad3 = Lenovo("IdeaPad 3 15IAU7", "Intel Core i5-1235U", ram=16, hdd=0, ssd=512, display=15.6, price=25999)
     sonyVaio = Sony("VAIO SVF1521A7EB", "Intel Pentium 2117U", ram=4, hdd=500, display=15.6, price=5480)
+    lenovoIdeaPad3 = Lenovo("IdeaPad 3 15IAU7", "Intel Core i5-1235", ram=16, hdd=0, ssd=512, display=15.6, price=25999)
 
-    lenovoIdeaPad3.print_characteristics()
-    print()
-    sonyVaio.print_characteristics()
+    print(sonyVaio.get_characteristics())
+    print(lenovoIdeaPad3.get_characteristics())
